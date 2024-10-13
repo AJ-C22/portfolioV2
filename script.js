aa8f2a852cb9c25a78a22d4f977f0706e9afb99e
Ajai Chandi <ajaischandi@gmail.com>
2024-10-13 19:31:02 -0400

    Finished up info with footer

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var cursor = document.querySelector('.blob');
+const blob = document.querySelector('.blob');
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+document.addEventListener('mousemove', (event) => {
+  const { clientX, clientY } = event;
+
+  // Get the body’s left and top padding values
+  const bodyStyles = getComputedStyle(document.body);
+  const paddingLeft = parseFloat(bodyStyles.paddingLeft);
+  const paddingTop = parseFloat(bodyStyles.paddingTop);
+
+  // Blob width and height for centering
+  const blobWidth = blob.offsetWidth / 2;
+  const blobHeight = blob.offsetHeight / 2;
+
+  // Adjust position to account for padding
+  const x = clientX - blobWidth - paddingLeft;
+  const y = clientY - blobHeight - paddingTop;
+
+  blob.style.transform = `translate(${x}px, ${y}px)`;
 });
